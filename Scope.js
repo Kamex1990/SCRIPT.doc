fe34b3e901b834637de77c7bd311a5d1f15a3b3b
Kamex1990 <82541968+Kamex1990@users.noreply.github.com>
2021-05-29 16:28:30 -0400

    Script.doc

---
+++ Scope.js
@@ -0,0 +1,74 @@
+
+
+// let  + const
+
+// const player = 'Bobby';
+// let experience = 100;
+// let wizardLevel = false;
+// if(experience > 90){
+// 	let wizardLevel = true;
+// }
+
+
+
+// const obj = {
+// 	player: 'Bobby',
+// 	experience: 100,
+// 	wizardLevel: false
+
+// }
+
+
+// Destructuring
+
+// const obj = {
+// 	player: 'Bobby',
+// 	experience: 100,
+// 	wizardLevel: false
+
+// }
+
+// const player = obj.player;
+// const experience = obj.experience;
+// let wizardLevel = obj.wizardLevel;
+
+
+// object properties
+
+// const name = 'John snow',
+// const obj = {
+// 	[name]: 'hello',
+// 	['ray' + 'Smith']: 'hihihi'
+// };
+
+// Template String
+
+// const name = 'Sally';
+// const age = 34;
+// const pet = 'horse';
+
+// const greetingBest = `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have!`;
+
+
+// Default argument
+
+// function greet(name = " ", age = 30, pet = 'cat'){
+// 	return `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have!`;
+
+// }
+
+
+// Symbol
+
+// let sym1 = symbol();
+// let sym2 = symbol('foo');
+// let sym3 = symbol('foo');
+
+// Arrow function
+
+
+function multiply(a, b){
+	return a * b;
+}
+
+const mult2 = (a, b) => a * b;
